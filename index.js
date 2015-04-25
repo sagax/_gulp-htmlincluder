274c8109162d2837897d606f3aa55038d89c7313
sagax <sagax@inbox.ru>
2015-04-25 14:34:15 +0300

    code brushed

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-var through = require("through2"),
-	gutil = require("gulp-util"),
-	includer = require("./lib/htmlincluder");
+var through  = require('through2');
+var gutil    = require('gulp-util');
+var includer = require('./lib/htmlincluder');
 
-//
-// @insertText = (optional) the test looked for in order to insert files 
-// 					(this is so ssi includes can be used instead)
-//
 module.exports = function (insertText) {
-	"use strict";
-	var that;
+  'use strict';
+  var that;
+  includer.initialize();
 
-	includer.initialize();
+  function htmlincluder() {
+    includer.buildHtml(function(file) {
+      var f = file.file;
+      f.contents = new Buffer(file.content);
+        that.push(f);
+    });
+  }
 
-	function htmlincluder() {
-		includer.buildHtml(function(file) {
-			var f = file.file;
-			f.contents = new Buffer(file.content);
+  function aggregateFiles(file, enc, callback) {
+    that = this;
 
-    		that.push(f);
-		});
-	}
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
 
-	function aggregateFiles(file, enc, callback) {
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-htmlincluder', 'Stream content is not supported'));
+      return callback();
+    }
 
-		that = this; //defined in scope of module
+    if (file.isBuffer()) {
+      includer.hashFile(file, insertText);
+    }
 
-		// Do nothing if no contents
-		if (file.isNull()) {
-			this.push(file);
-			return callback();
-		}
+    return callback();
+  }
 
-		if (file.isStream()) {
-			this.emit("error",
-				new gutil.PluginError("gulp-htmlincluder", "Stream content is not supported"));
-			return callback();
-		}
-
-		if (file.isBuffer()) {
-			includer.hashFile(file, insertText);
-		}
-
-		return callback();
-	}
-
-	return through.obj(aggregateFiles, htmlincluder);
+  return through.obj(aggregateFiles, htmlincluder);
 };
